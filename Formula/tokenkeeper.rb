0a10c4f3a68ac47a2b6d69e4114ae3166fd0c91c
Egor Merkushev <gorkaedeep@gmail.com>
2026-07-12 10:31:48 +0300

    Select architecture-specific Homebrew source

--- Formula/tokenkeeper.rb
+++ Formula/tokenkeeper.rb
@@ -3,12 +3,10 @@ class Tokenkeeper < Formula
   homepage "https://github.com/SoundBlaster/tokenkeeper"
   license "MIT"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/SoundBlaster/tokenkeeper/releases/download/v0.2.2/tokenkeeper-v0.2.2-aarch64-apple-darwin.tar.gz"
     sha256 "f1e5fd6d73b4d895d5ccc21ce0574568c55c7702d972accf89362e7a2c1b7b9b"
-  end
-
-  on_intel do
+  else
     url "https://github.com/SoundBlaster/tokenkeeper/archive/refs/tags/v0.2.2.tar.gz"
     sha256 "2f76f4b19cb57bb1088461e12a2cc66f6a3300fc36875f706d23bb53c5264b96"
     depends_on "rust" => :build
